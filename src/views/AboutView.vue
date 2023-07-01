a4f5c52c377df8579649ef24ded65edea6fb4299
alexis <alexislezcano79@gmail.com>
2023-07-01 12:10:30 -0300

    Subiendo proyecto Todo

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1 class="pa-3">Bienvenido a tu lista de tareas </h1>
+    <p class="pa-3">¡Presentamos nuestra innovadora aplicación de lista de tareas! Diseñada para simplificar tu vida ocupada, nuestra aplicación te brinda una forma eficiente de organizar y gestionar todas tus tareas diarias.</p>
+    <img src="../img/ejemplo.jpg" alt="">
+    <p class="pa-3">Ya sea que necesites recordar tus tareas pendientes, establecer recordatorios o establecer fechas límite, nuestra aplicación intuitiva y fácil de usar es tu compañero ideal para mantenerte en el camino hacia el éxito.</p>
+    
+    <p class="pa-3">Con características personalizables, notificaciones en tiempo real y una interfaz amigable, nunca antes había sido tan fácil mantener todas tus responsabilidades en orden. ¡Descarga nuestra aplicación ahora y experimenta una forma completamente nueva de administrar tus tareas!</p>
+
+    
+    
+  </div>
+</template>
